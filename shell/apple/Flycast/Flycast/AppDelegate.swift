f82ec35339ea785f1c66d5a7cc398771d9e1ad8b
Edward Li <vk.edward.li@gmail.com>
2020-09-30 04:52:59 +0800

    Dummy macOS project

---
+++ shell/apple/Flycast/Flycast/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Flycast
+//
+//  Created by Edward Li on 30/9/2020.
+//  Copyright © 2020 Edward Li. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ shell/apple/Flycast/Flycast/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Flycast
+//
+//  Created by Edward Li on 30/9/2020.
+//  Copyright © 2020 Edward Li. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
